24dac4c80a281560e303d89352525f929008d964
Dave Mattice <david.mattice@hotmail.com>
2023-12-19 18:23:16 -0500

    basics

--- app.py
+++ app.py
@@ -9,9 +9,26 @@ import os
 from flask import Flask, render_template, request, url_for, flash, redirect
 from screenlogicpy.gateway import ScreenLogicGateway
 
+gateway = None
+
+app = Flask(__name__)
+
 @app.route('/', methods=['GET', 'POST'])
 async def index():
-    return render_template('index.html')
+  global gateway
+  ip = "192.168.1.156"
+  hosts = [{"ip": ip, "port": "80"}]
+  success = None
+  msg = "Not connected!"
+
+  success = await gateway.async_connect(**hosts[0])
+  if success:
+    msg = "Connected!"
+    success = await gateway.async_update()  
+    msg = gateway.get_data("controller", "sensor", "air_temperature", "value")
+    success = await gateway.async_disconnect()
+
+  return render_template('index.html', msg=msg)
 
 
 if __name__ == "app":
